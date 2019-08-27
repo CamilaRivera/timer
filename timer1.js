4cedce1845d5d2badd5ae7264a54d4497696f8f1
CamilaRivera <camila.riverart@gmail.com>
2019-08-27 21:33:36 +0000

    Timer eslint refactor with @SarahMahovlich

--- timer1.js
+++ timer1.js
@@ -1,13 +1,13 @@
 const timer = process.argv.slice(2);
 
-const alarm = function (soundType) {
+const alarm = function(soundType) {
   process.stdout.write(soundType);
 };
 
 timer.forEach((timerInput) => {
-  if(Number(timerInput) && timerInput >= 0){
+  if (Number(timerInput) && timerInput >= 0) {
     setTimeout(() => {
       alarm('\x07');
-    }, timerInput * 1000)
-  }  
+    }, timerInput * 1000);
+  }
 });
